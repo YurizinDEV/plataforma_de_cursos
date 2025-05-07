6eb1801fa53835e879dda3cac95962f42ba5a4e8
YurizinDEV <yurizetoles0123@gmail.com>
2025-05-07 17:13:41 -0300

    Remove código comentado duplicado da classe Alternativa

--- src/models/Alternativa.js
+++ src/models/Alternativa.js
@@ -39,3 +39,14 @@ class Alternativa {
         });  
     }  
 }  */
+
+    /*class Alternativa {  
+    constructor() {  
+        const alternativaSchema = new mongoose.Schema({  
+            ale_id: { type: Number, index: true, required: true },  
+            ale_texto: { type: String, required: true, maxlength: 250 },  
+            ale_numero: { type: Number, required: true },  
+            ale_ques_id_quest_fk: { type: Number, required: true }  
+        });  
+    }  
+}  */
