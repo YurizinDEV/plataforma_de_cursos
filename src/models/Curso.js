94af8449a6afbf6f2d8b1aa577ea48f545e68ba2
YurizinDEV <yurizetoles0123@gmail.com>
2025-05-10 15:32:07 -0300

    criação do model Curso com esquema e suporte a paginação

---
+++ src/models/Curso.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+// Curso Class  
+
+class Curso {
+    constructor() {
+        const cursoSchema = new mongoose.Schema({
+            titulo: {
+                type: String,
+                required: true,
+                maxlength: 100
+            },
+            descricao: {
+                type: String
+            },
+            criadoEm: {
+                type: Date,
+                default: Date.now
+            },
+            thumbnail: {
+                type: String,
+                maxlength: 250
+            },
+            cargaHorariaTotal: {
+                type: Number,
+                required: true,
+                min: 1
+            },
+            materialComplementar: {
+                type: [String]
+            },
+            professores: {
+                type: [String]
+            },
+            tags: {
+                type: [String]
+            },
+            criadoPorId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Usuario",
+                required: true
+            }
+        }, {
+            versionKey: false
+        });
+
+        cursoSchema.plugin(mongoosePaginate);
+        this.model = mongoose.model("Curso", cursoSchema);
+    }
+}
+
+export default new Curso().model;
