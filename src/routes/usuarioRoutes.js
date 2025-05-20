b48dbec4b443c3540552283b86477ea1909dcb01
YurizinDEV <yurizetoles0123@gmail.com>
2025-05-20 18:16:12 -0300

    refactor: ajusta comentários e formatação nas rotas de usuário

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,19 +1,20 @@
 import express from "express";
+// Importando middleware de autenticação e autorização, se necessário
 // import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 // import authPermission from '../middlewares/AuthPermission.js';
 import UsuarioController from '../controllers/UsuarioController.js';
 import { asyncWrapper } from '../utils/helpers/index.js';
 
 const router = express.Router();
-
 const usuarioController = new UsuarioController(); 
 
+// Definindo as rotas com o controlador e os middlewares necessários
 router
-    .get("/usuarios", /*AuthMiddleware, *authPermission,*/ asyncWrapper(usuarioController.listar.bind(usuarioController)))
+    .get("/usuarios", /*AuthMiddleware, authPermission,*/ asyncWrapper(usuarioController.listar.bind(usuarioController)))
     .get("/usuarios/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(usuarioController.listar.bind(usuarioController)))
     .post("/usuarios", /*AuthMiddleware, authPermission,*/ asyncWrapper(usuarioController.criar.bind(usuarioController)))
     .patch("/usuarios/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(usuarioController.atualizar.bind(usuarioController)))
     .put("/usuarios/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(usuarioController.atualizar.bind(usuarioController)))
-    .delete("/usuarios/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(usuarioController.deletar.bind(usuarioController)))
+    .delete("/usuarios/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(usuarioController.deletar.bind(usuarioController)));
 
 export default router;
