2261510781b77b43af6b3c4bf79f85813bcf049a
YurizinDEV <yurizetoles0123@gmail.com>
2025-05-20 20:27:17 -0400

    refactor: corrige a importação do DbConnect e melhora a legibilidade do código em app.js

--- src/app.js
+++ src/app.js
@@ -3,38 +3,24 @@ import routes from "./routes/index.js";
 import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
-import DbConnect from './config/dbConnect.js';
+import DbConnect from './config/DbConnect.js';
 import errorHandler from './utils/helpers/errorHandler.js';
 import logger from './utils/logger.js';
 import CommonResponse from './utils/helpers/CommonResponse.js';
 
 const app = express();
 
-// Configura o middleware express-fileupload
-
-
-// Conectando ao banco de dados
 await DbConnect.conectar();
 
-// Middlewares de segurança
 app.use(helmet());
-
-// Habilitando CORS
 app.use(cors());
-
-// Habilitando a compressão de respostas
 app.use(compression());
-
-// Habilitando o uso de json pelo express
 app.use(express.json());
-
-// Habilitando o uso de urlencoded pelo express
 app.use(express.urlencoded({ extended: true }));
 
-// Passando para o arquivo de rotas o app
 routes(app);
 
-// Middleware para lidar com rotas não encontradas (404)
+// Middleware para lidar com rotas não encontradas (404).
 app.use((req, res, next) => {
     return CommonResponse.error(
         res,
@@ -45,22 +31,16 @@ app.use((req, res, next) => {
     );
 });
 
-
-// Listener para erros não tratados (opcional, mas recomendado)
+// Listeners para erros não tratados.
 process.on('unhandledRejection', (reason, promise) => {
     logger.error('Unhandled Rejection at:', promise, 'reason:', reason);
-    // Não finalizar o processo para evitar interrupção da API
 });
 
 process.on('uncaughtException', (error) => {
     logger.error('Uncaught Exception thrown:', error);
-    // Não finalizar o processo para evitar interrupção da API
-    // Considerar reiniciar a aplicação em caso de exceções críticas
 });
 
-// Middleware de Tratamento de Erros (deve ser adicionado após as rotas)
+// Middleware de Tratamento de Erros (deve ser adicionado após as rotas).
 app.use(errorHandler);
 
-// exportando para o server.js fazer uso
 export default app;
-
