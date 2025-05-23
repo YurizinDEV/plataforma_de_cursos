7fc2f980d4f909bfcd2560d708be9aa27b747a08
YurizinDEV <yurizetoles0123@gmail.com>
2025-05-23 17:39:29 -0300

    refactor: atualiza rotas de cursos para métodos mais claros e consistentes

--- src/routes/cursoRoutes.js
+++ src/routes/cursoRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
-import CursoController from '../controllers/CursoController.js'; // Controller para gestão de cursos
+import CursoController from '../controllers/CursoController.js';
 import { asyncWrapper } from '../utils/helpers/index.js';
 
 const router = express.Router();
 const cursoController = new CursoController();
 
-router.get("/cursos", asyncWrapper(cursoController.listarCursos.bind(cursoController)));
-router.get("/curso/:id", asyncWrapper(cursoController.visualizarCurso.bind(cursoController)));
-router.post("/curso/:id/matricula", asyncWrapper(cursoController.matricularAluno.bind(cursoController)));
+router.post('/cursos', asyncWrapper(cursoController.criar.bind(cursoController))); // Criar curso
+router.get('/cursos', asyncWrapper(cursoController.listar.bind(cursoController))); // Listar cursos
+router.put('/cursos/:id', asyncWrapper(cursoController.atualizar.bind(cursoController))); // Atualizar curso
+router.delete('/cursos/:id', asyncWrapper(cursoController.deletar.bind(cursoController))); // Deletar curso
 
 export default router;
