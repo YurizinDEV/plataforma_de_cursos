95ee7dc5ee9c3b75f0aae6a5e4a5636fc8dd6db6
YurizinDEV <yurizetoles0123@gmail.com>
2025-05-24 13:14:16 -0300

    feat(usuarios): implementa método de criação de usuários com remoção segura de senha
    Adiciona o método criar() no UsuarioController que:
    - Valida os dados do usuário com UsuarioSchema
    - Envia os dados para o serviço correspondente
    - Remove a senha do objeto retornado antes de enviá-lo ao cliente
    - Retorna resposta HTTP 201 com os dados do usuário criado

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,9 +1,19 @@
 //UsuarioController.js
 
 import UsuarioService from '../services/UsuarioService.js';
-import { UsuarioQuerySchema, UsuarioIdSchema } from '../utils/validators/schemas/zod/querys/UsuarioQuerySchema.js';
-import { UsuarioSchema, UsuarioUpdateSchema } from '../utils/validators/schemas/zod/UsuarioSchema.js';
-import { CommonResponse, CustomError, HttpStatusCodes } from '../utils/helpers/index.js';
+import {
+    UsuarioQuerySchema,
+    UsuarioIdSchema
+} from '../utils/validators/schemas/zod/querys/UsuarioQuerySchema.js';
+import {
+    UsuarioSchema,
+    UsuarioUpdateSchema
+} from '../utils/validators/schemas/zod/UsuarioSchema.js';
+import {
+    CommonResponse,
+    CustomError,
+    HttpStatusCodes
+} from '../utils/helpers/index.js';
 
 class UsuarioController {
     constructor() {
@@ -11,7 +21,9 @@ class UsuarioController {
     }
 
     async listar(req, res) {
-        const { id } = req.params || {};
+        const {
+            id
+        } = req.params || {};
         if (id) {
             UsuarioIdSchema.parse(id);
         }
@@ -23,6 +35,17 @@ class UsuarioController {
 
         const data = await this.service.listar(req);
         return CommonResponse.success(res, data);
+    }
+
+    async criar(req, res) {
+        const parsedData = UsuarioSchema.parse(req.body);
+
+        const data = await this.service.criar(parsedData);
+
+        const usuarioLimpo = data.toObject();
+        delete usuarioLimpo.senha;
+
+        return CommonResponse.created(res, usuarioLimpo);
     }
 }
 
