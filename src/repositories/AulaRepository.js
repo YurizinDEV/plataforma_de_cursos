170817f454f726f9efcdd14ae9a4dfd6d35fba8e
Aílton Vieira Gonçalves <vgailton20@gmail.com>
2025-05-25 13:53:05 -0400

    Criação do Repository e filter referentes a AulaRepository.js

---
+++ src/repositories/AulaRepository.js
@@ -0,0 +1,46 @@
+import Aula from "../models/Aula.js";
+import AulaFilterBuilder from "./filters/AulaFilterBuilder.js";
+
+class AulaRepository {
+    async criar(aulaData) {
+        const novaAula = new Aula(aulaData);
+        return await novaAula.save();
+    }
+
+    async verificarExistenciaPorCurso(cursoId, titulo) {
+        return await Aula.findOne({ cursoId, titulo });
+    }
+
+    async listar(filters = {}) {
+        const query = new AulaFilterBuilder()
+            .porTitulo(filters.titulo)
+            .porCursoId(filters.cursoId)
+            .porCriadorId(filters.criadorId)
+            .comCargaHorariaMinima(filters.cargaMinima)
+            .comCargaHorariaMaxima(filters.cargaMaxima)
+            .build();
+
+        return await Aula.find(query);
+    }
+
+    async listarPaginado({ page = 1, limit = 10, ...filters }) {
+        const query = new AulaFilterBuilder()
+            .porTitulo(filters.titulo)
+            .porCursoId(filters.cursoId)
+            .porCriadorId(filters.criadorId)
+            .comCargaHorariaMinima(filters.cargaMinima)
+            .comCargaHorariaMaxima(filters.cargaMaxima)
+            .comMaterialComplementar()
+            .build();
+
+        const options = {
+            page: parseInt(page),
+            limit: parseInt(limit),
+            sort: { createdAt: -1 }
+        };
+
+        return await Aula.paginate(query, options);
+    }
+}
+
+export default new AulaRepository();
