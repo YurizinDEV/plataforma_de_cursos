e11ebcfb7f324e2fa1374e3b47f87bfbac01bca0
André Lucas <andrelusantosa@gmail.com>
2025-05-25 23:21:01 -0400

    Repositório e FilterBuilder  de Questionário.

---
+++ src/repositories/QuestionarioRepository.js
@@ -0,0 +1,59 @@
+import Questionario from "../models/Questionario.js";
+import QuestionarioFilterBuilder from "./filters/QuestionarioFilterBuilder.js";
+
+class QuestionarioRepository {
+  constructor() {
+    this.model = Questionario;
+  }
+
+  async criar(questionarioData) {
+    return await this.model.create(questionarioData);
+  }
+
+  async buscarPorId(id) {
+    return await this.model.findById(id).populate('alternativas');
+  }
+
+  async listar(filters = {}) {
+    const query = new QuestionarioFilterBuilder()
+      .porAulaId(filters.aulaId)
+      .build();
+
+    return await this.model.find(query).populate('alternativas');
+  }
+
+  async listarPaginado({ page = 1, limit = 10, ...filters }) {
+    const query = new QuestionarioFilterBuilder()
+      .porAulaId(filters.aulaId)
+      .build();
+
+    return await this.model.paginate(query, {
+      page,
+      limit,
+      sort: { createdAt: -1 },
+      populate: 'alternativas'
+    });
+  }
+
+  async atualizar(id, dadosAtualizados) {
+    return await this.model.findByIdAndUpdate(
+      id, 
+      dadosAtualizados, 
+      { new: true }
+    ).populate('alternativas');
+  }
+
+  async deletar(id) {
+    return await this.model.findByIdAndDelete(id);
+  }
+
+  async adicionarAlternativa(questionarioId, alternativaId) {
+    return await this.model.findByIdAndUpdate(
+      questionarioId,
+      { $push: { alternativas: alternativaId } },
+      { new: true }
+    ).populate('alternativas');
+  }
+}
+
+export default new QuestionarioRepository();
