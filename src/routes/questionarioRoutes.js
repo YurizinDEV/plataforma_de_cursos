62b2e27b67e79ca12b9fe2210f4dbd1ffeecec63
André Lucas <andrelusantosa@gmail.com>
2025-05-25 23:51:12 -0400

    Criação questionarioRoutes.js.

---
+++ src/routes/questionarioRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import QuestionarioController from "../controllers/QuestionarioController.js";
+import { asyncWrapper } from '../utils/helpers/index.js';
+
+const router = express.Router();
+const questionarioController = new QuestionarioController();
+
+router
+  .get("/questionarios", asyncWrapper(questionarioController.listar.bind(questionarioController)))
+  .get("/questionarios/paginado", asyncWrapper(questionarioController.listarPaginado.bind(questionarioController)))
+  .get("/questionarios/:id", asyncWrapper(questionarioController.buscar.bind(questionarioController)))
+  .post("/questionarios", asyncWrapper(questionarioController.criar.bind(questionarioController)))
+  .put("/questionarios/:id", asyncWrapper(questionarioController.atualizar.bind(questionarioController)))
+  .delete("/questionarios/:id", asyncWrapper(questionarioController.deletar.bind(questionarioController)))
+  .post("/questionarios/:questionarioId/alternativas/:alternativaId", 
+    asyncWrapper(questionarioController.adicionarAlternativa.bind(questionarioController)));
+
+export default router;
