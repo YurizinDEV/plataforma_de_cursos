2f839c0e4242170eef130147d3804d804c258373
YurizinDEV <yurizetoles0123@gmail.com>
2025-05-27 11:39:36 -0300

    feat(rotas): ativa rota para atualização de cursos no cursoRoutes

--- src/routes/cursoRoutes.js
+++ src/routes/cursoRoutes.js
@@ -12,7 +12,8 @@ const cursoController = new CursoController();
 router.post('/cursos', /*AuthMiddleware, authPermission,*/ asyncWrapper(cursoController.criar.bind(cursoController))); 
 router.get('/cursos',  /*AuthMiddleware, authPermission,*/ asyncWrapper(cursoController.listar.bind(cursoController)));
 router.get('/cursos/:id', /*AuthMiddleware, authPermission,*/ asyncWrapper(cursoController.listar.bind(cursoController))); 
-// router.put('/cursos/:id', /*AuthMiddleware, authPermission,*/ asyncWrapper(cursoController.atualizar.bind(cursoController)));
+router.put('/cursos/:id', /*AuthMiddleware, authPermission,*/ asyncWrapper(cursoController.atualizar.bind(cursoController)));
+router.patch('/cursos/:id', /*AuthMiddleware, authPermission,*/ asyncWrapper(cursoController.atualizar.bind(cursoController)));
 // router.delete('/cursos/:id', /*AuthMiddleware, authPermission,*/ asyncWrapper(cursoController.deletar.bind(cursoController))); 
 
 export default router;
